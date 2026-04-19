cd6a36518b3b310569e7a7d98b98b7fb17a83bcf
Abuawaish <74604342+abuawaish@users.noreply.github.com>
2026-04-20 02:54:07 +0530

    prevent -> Chromium crash error

--- myntra_scraper.py
+++ myntra_scraper.py
@@ -99,6 +99,11 @@ def get_driver():
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+    options.add_argument("--single-process")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-software-rasterizer")
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--allow-running-insecure-content")
 
     options.binary_location = "/usr/bin/chromium"
     service = Service("/usr/bin/chromedriver") 
